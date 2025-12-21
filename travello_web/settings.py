ddb6606a44dce3e11950f8823d2cf0f52e2d9d4e
Madhusudan04337 <madhusudan04337@gmail.com>
2025-12-21 17:25:57 +0530

    feat: Initialize Django project settings with basic configuration, installed apps, and static/media file setup.

--- travello_web/settings.py
+++ travello_web/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9_9@lf3@86kug%w71u^2y6-#x=j_-iui(pwqgi_09e#!#cmr0#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "travello-web-production.up.railway.app",
